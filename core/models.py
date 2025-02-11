e5f010645044a4d741593b9c5036cbeaf2ff6fc3
GauravChavan2004 <gauravchavan101004@gmail.com>
2025-02-11 09:47:33 +0530

    home page dynamic complete

--- core/models.py
+++ core/models.py
@@ -3,30 +3,47 @@ from django.contrib.auth.models import User
 import re
 class Car(models.Model):
     MAKE_CHOICES = [
-        ('MARUTI', 'Maruti'),
-        ('HYUNDAI', 'Hyundai'),
-        ('TATA', 'Tata'),
-        ('TOYOTA', 'Toyota'),
-        ('HONDA', 'Honda'),
-        ('FORD', 'Ford'),
-        ('KIA', 'Kia'),
+        ('Maruti', 'Maruti'),
+        ('Hyundai', 'Hyundai'),
+        ('Tata', 'Tata'),
+        ('Toyoto', 'Toyota'),
+        ('Honda', 'Honda'),
+        ('Ford', 'Ford'),
+        ('Kia', 'Kia'),
+        # Add more makes as needed
+    ]
+    reg_month_CHOICES = [
+        ('Jan', 'january'),
+        ('Feb', 'Febuary'),
+        ('March', 'March'),
+        ('April', 'April'),
+        ('May', 'may'),
+        ('June', 'June'),
+        ('July', 'July'),
+        ('Aug', 'August'),
+        ('Sep', 'September'),
+        ('Oct', 'October'),
+        ('Nov', 'November'),
+        ('Dec', 'December'),
         # Add more makes as needed
     ]
 
     
     make = models.CharField(max_length=20, choices=MAKE_CHOICES)
     model = models.CharField(max_length=100)
-    registration_year = models.CharField(max_length=10, help_text="Enter in the format: eg. Aug 2020")
+    registration_month = models.CharField(max_length=20, choices=reg_month_CHOICES, default="january")
+    registration_year = models.IntegerField()
     manufacture_year = models.IntegerField()
     owner = models.IntegerField()
     km_driven = models.IntegerField()
-    transmission_type = models.CharField(max_length=20, choices=[('MANUAL', 'Manual'), ('AUTOMATIC', 'Automatic')])
-    fuel_type = models.CharField(max_length=20, choices=[('PETROL', 'Petrol'), ('DIESEL', 'Diesel'),('PETROL+CNG', 'Petrol+cng'), ('ELECTRIC', 'Electric')])
+    transmission_type = models.CharField(max_length=20, choices=[('Manual', 'Manual'), ('Automatic', 'Automatic')])
+    fuel_type = models.CharField(max_length=20, choices=[('Petrol', 'Petrol'), ('Diesel', 'Diesel'),('Petrol+CNG', 'Petrol+CNG'), ('ELECTRIC', 'Electric')])
     insurance_validity = models.CharField(max_length=100)
-    insurance_type = models.CharField(max_length=20, choices=[('COMPREHENSIVE', 'Comprehensive'), ('THIRD-PARTY', 'Third-Party')])
+    insurance_type = models.CharField(max_length=20, choices=[('Comprehensive', 'Comprehensive'), ('Third-Party', 'Third-Party')])
     RTO = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     car_location = models.CharField(max_length=100)
+    car_img=models.ImageField(upload_to='title_car/',default='media\default_img\default_img.png', help_text="Upload an image of the car for the display view.")
     status = models.CharField(max_length=20, choices=[('AVAILABLE', 'Available'), ('BOOK', 'Book'),('SOLD', 'Sold')], default="AVAILABLE")
 
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
-from django.http import HttpResponse
+from core.models import Car
 
 
 # Create your views here.
@@ -8,7 +7,22 @@ def index(request):
     return render(request,"core/index.html")
 
 def home_page(request):
-    return render(request,"core/home_page.html")
+    data = Car.objects.all()
+    formatted_data = []
+    
+    for car in data:
+        lakh_price = car.price / 100000  # Convert the price to lakh
+        formatted_price = "{:.2f} lakh".format(lakh_price)  # Format the price
+        print(formatted_price)
+        formatted_data.append({
+            'car': car,
+            'formatted_price': formatted_price
+        })
+    
+    # Pass only the formatted data to the template
+    return render(request, "core/home_page.html", {'formatted_data': formatted_data})
+
 
 def car_view(request):
+    data = Car.objects.all()
     return render(request, "car_view\car_view.html")
